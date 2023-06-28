446240956488e2eccc66173fbb123e93f873553d
Jubi <ayshajubin000@gmail.com>
2023-06-28 15:22:02 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Header";
+import { BrowserRouter, Route, Routes, Switch} from 'react-router-dom';
 
 function App() {
+  
   return (
     <div className="App">
-      Welcome
+     <Header />
+     <Routes>
+      <Route path="/" element={<Dashboard />}></Route>
+     </Routes>
+   
     </div>
   );
 }
-
 export default App;
